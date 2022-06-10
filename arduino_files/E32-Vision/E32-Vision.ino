e17675c3b897e367a3dc24514a985cdbbf173106
admin@example.com <00002918@uca.edu.sv>
2022-06-09 20:00:59 -0600

    Finalizacion de captura de imagenes

--- arduino_files/E32-Vision/E32-Vision.ino
+++ arduino_files/E32-Vision/E32-Vision.ino
@@ -1,10 +1,13 @@
-/*
-  Complete details at https://RandomNerdTutorials.com/esp32-useful-wi-fi-functions-arduino/
-*/
-
-//Testing wifi
-
+#include "esp_camera.h"
 #include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h" 
+#include "soc/rtc_cntl_reg.h"  
+#include "esp_http_server.h"
+
 
 // Replace with your network credentials (STATION)
 const char* ssid     = "nombre_red";
@@ -30,7 +33,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
   while(true){
     fb = esp_camera_fb_get();
     if (!fb) {
-      Serial.println("Camera capture failed");
+      Serial.println("Captura fallida");
       res = ESP_FAIL;
     } else {
       if(fb->width > 400){
@@ -39,7 +42,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
           esp_camera_fb_return(fb);
           fb = NULL;
           if(!jpeg_converted){
-            Serial.println("JPEG compression failed");
+            Serial.println("Fallo comprension de imagen");
             res = ESP_FAIL;
           }
         } else {
@@ -88,6 +91,46 @@ void startCameraServer(){
 
   
 void setup(){
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Inizialicacion de camara
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Error de inicializacion de camara 0x%x", err);
+    return;
+  }
+
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -95,8 +138,15 @@ void setup(){
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected.");
+  Serial.println("WiFi conectado");
   delay(100);
+
+
+  Serial.print("Stream listo Ingresar a : http://");
+  Serial.print(WiFi.localIP());
+  
+  // Inicializar el servidor web
+  startCameraServer();
 }
 
 void loop(){
