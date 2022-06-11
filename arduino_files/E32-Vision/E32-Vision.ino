923b828996d2c9dd8f47de40e98bc5fa4db3edb8
DouglasLino <54186619+DouglasLino@users.noreply.github.com>
2022-06-11 17:16:25 -0600

    Mejor funcionamiento de camara y VCL

--- arduino_files/E32-Vision/E32-Vision.ino
+++ arduino_files/E32-Vision/E32-Vision.ino
@@ -1,96 +1,120 @@
-#include "esp_camera.h"
+/*
+  This is a simple MJPEG streaming webserver implemented for AI-Thinker ESP32-CAM and
+  ESP32-EYE modules.
+  This is tested to work with VLC and Blynk video widget.
+  Inspired by and based on this Instructable: $9 RTSP Video Streamer Using the ESP32-CAM Board
+  (https://www.instructables.com/id/9-RTSP-Video-Streamer-Using-the-ESP32-CAM-Board/)
+  Board: AI-Thinker ESP32-CAM
+*/
+
+#include "src/OV2640.h"
 #include <WiFi.h>
-#include "esp_timer.h"
-#include "img_converters.h"
-#include "Arduino.h"
-#include "fb_gfx.h"
-#include "soc/soc.h" 
-#include "soc/rtc_cntl_reg.h"  
-#include "esp_http_server.h"
+#include <WebServer.h>
+#include <WiFiClient.h>
 
 
-// Replace with your network credentials (STATION)
-const char* ssid     = "nombre_red";
-const char* password = "contraseña";
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+#define CAMERA_MODEL_AI_THINKER
 
-httpd_handle_t stream_httpd = NULL;
 
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
+#if defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Modelo de la camara no seleccionado"
+#endif
 
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
-    return res;
+// wifi network name
+#define SSID1 "CLARO1_1BD04E"
+
+// wifi network password
+#define PWD1 "832W0hJSFc"
+
+
+OV2640 cam;
+
+WebServer server(80);
+
+const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
+                      "Access-Control-Allow-Origin: *\r\n" \
+                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
+const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
+const int hdrLen = strlen(HEADER);
+const int bdrLen = strlen(BOUNDARY);
+const int cntLen = strlen(CTNTTYPE);
+
+void handle_jpg_stream(void)
+{
+  char buf[32];
+  int s;
+
+  WiFiClient client = server.client();
+
+  client.write(HEADER, hdrLen);
+  client.write(BOUNDARY, bdrLen);
+
+  while (true)
+  {
+    if (!client.connected()) break;
+    cam.run();
+    s = cam.getSize();
+    client.write(CTNTTYPE, cntLen);
+    sprintf( buf, "%d\r\n\r\n", s );
+    client.write(buf, strlen(buf));
+    client.write((char *)cam.getfb(), s);
+    client.write(BOUNDARY, bdrLen);
   }
-
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Captura fallida");
-      res = ESP_FAIL;
-    } else {
-      if(fb->width > 400){
-        if(fb->format != PIXFORMAT_JPEG){
-          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-          esp_camera_fb_return(fb);
-          fb = NULL;
-          if(!jpeg_converted){
-            Serial.println("Fallo comprension de imagen");
-            res = ESP_FAIL;
-          }
-        } else {
-          _jpg_buf_len = fb->len;
-          _jpg_buf = fb->buf;
-        }
-      }
-    }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    }
-    if(fb){
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      _jpg_buf = NULL;
-    } else if(_jpg_buf){
-      free(_jpg_buf);
-      _jpg_buf = NULL;
-    }
-    if(res != ESP_OK){
-      break;
-    }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
-  }
-  return res;
 }
 
+const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
+                       "Content-disposition: inline; filename=capture.jpg\r\n" \
+                       "Content-type: image/jpeg\r\n\r\n";
+const int jhdLen = strlen(JHEADER);
 
-void startCameraServer(){
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-  config.server_port = 80;
+void handle_jpg(void)
+{
+  WiFiClient client = server.client();
 
-  httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
+  cam.run();
+  if (!client.connected()) return;
 
-  
-void setup(){
+  client.write(JHEADER, jhdLen);
+  client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text / plain", message);
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  //while (!Serial);            //wait for serial connection.
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -112,45 +136,45 @@ void setup(){
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
-  
-  // Inizialicacion de camara
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Error de inicializacion de camara 0x%x", err);
-    return;
-  }
+  config.pixel_format = PIXFORMAT_JPEG;
 
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  // Frame parameters
+  config.frame_size = FRAMESIZE_UXGA;
+//  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  cam.init(config);
+
+  IPAddress ip;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID1, PWD1);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
-    Serial.print(".");
+    Serial.print(F("."));
   }
+  ip = WiFi.localIP();
+  Serial.println(F("WiFi conectado"));
   Serial.println("");
-  Serial.println("WiFi conectado");
-  delay(100);
-
-
-  Serial.print("Stream listo Ingresar a : http://");
-  Serial.print(WiFi.localIP());
-  
-  // Inicializar el servidor web
-  startCameraServer();
+//  Serial.println(ip);
+  Serial.print("Direccion ip: http://");
+  Serial.print(ip);
+//  Serial.println("/mjpeg/1");
+//  server.on("/mjpeg/1", HTTP_GET, handle_jpg_stream);
+  server.on("/", HTTP_GET, handle_jpg_stream);
+  server.on("/jpg", HTTP_GET, handle_jpg);
+  server.onNotFound(handleNotFound);
+  server.begin();
 }
 
-void loop(){
-  Serial.print("RSSI: ");
-  Serial.println(WiFi.RSSI());
-  delay(2000);
+void loop()
+{
+  server.handleClient();
 }
